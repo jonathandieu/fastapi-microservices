9d51800c7ade57a93eb0f0e64d578fb6e931cbf2
Jonathan Dieu <jonathanhuydieu@gmail.com>
2023-02-05 22:05:12 -0900

    Add Product class and attributes

--- inventory/main.py
+++ inventory/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
-from redis_om import get_redis_connection
+from redis_om import get_redis_connection, HashModel
 
 app = FastAPI()
+
+class Product(HashModel):
+    name: str
+    price: float
+    quantity: int
 
 @app.get("/")
 async def root():
