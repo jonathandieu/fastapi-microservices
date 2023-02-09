9e28c052b1bf56ec9df4ec46362fa49b6842522a
Jonathan Dieu <jonathanhuydieu@gmail.com>
2023-02-08 22:18:41 -0900

    Add CORS import and product GET request

--- inventory/main.py
+++ inventory/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from redis_om import get_redis_connection, HashModel
+from fastapi.middleware.cors import CORDSMiddleware
 
 app = FastAPI()
 
@@ -11,3 +12,7 @@ class Product(HashModel):
 @app.get("/")
 async def root():
     return {"message" : "testing!"}
+
+@app.get("/products")
+def all():
+    return Product.all_pks()
